051764fe5d01580c75f8ac7a44a3a58862a26831
Tanner Rigby <tanner.rigby@canopytax.com>
2019-10-29 11:26:06 -0600

    change appt model

--- models/appointment.js
+++ models/appointment.js
@@ -1,48 +1,49 @@
-import _ from 'lodash';
+import _ from "lodash";
 
 const appointment = {
-    appointmentId: '',
-    clientId: '',
-    service: '',
-    productUsed: '',
-    followUpDate: '',
-    followUpTime: '',
-    amountPaid: '',
-    discountType: '',
-    discountAmount: '',
-    tip: '',
-    time: '',
-    duration: '',
-    date: '',
-    milesDriven: '',
-    location: '',
-    notes: '',
-    retailItemsSold: '',
-    retailItemsAmount: '',
-    createdAt: '',
-    updatedAt: '',
-  };
+  appointmentId: "",
+  clientId: "",
+  amountPaid: "",
+  apptStatus: "",
+  date: "",
+  discountAmount: "",
+  discountType: "",
+  duration: "",
+  followUpDate: "",
+  followUpTime: "",
+  location: "",
+  milesDriven: "",
+  notes: "",
+  productUsed: "",
+  retailItemsAmount: "",
+  retailItemsSold: "",
+  service: "",
+  time: "",
+  tip: "",
+  createdAt: "",
+  updatedAt: "",
+};
 
-  export function getAppointmentKeys() {
-    return Object.keys(appointment);
-  }
-  
-  export function getAppointment(data) {
-    const appointmentKeys = getAppointmentKeys();
-    const prunedData = _.cloneDeep(data);
-  
-    _.forIn(data, (value, key) => {
-      // check if key exists on appointment object
-      const index = _.indexOf(appointmentKeys, key);
-  
-      if (index < 0) {
-        // It doesn't exist, don't allow it to be added to db
-        delete prunedData[key];
-      } else if (!value) {
-        // No value, delete it
-        delete prunedData[key];
-      }
-    });
-  
-    return prunedData;
-  }
+export function getAppointmentKeys() {
+  return Object.keys(appointment);
+}
+
+export function getAppointment(data) {
+  const appointmentKeys = getAppointmentKeys();
+  const prunedData = _.cloneDeep(data);
+
+  _.forIn(data, (value, key) => {
+    // check if key exists on appointment object
+    const index = _.indexOf(appointmentKeys, key);
+
+    if (index < 0) {
+      // It doesn't exist, don't allow it to be added to db
+      delete prunedData[key];
+    } else if (!value) {
+      // No value, delete it
+      delete prunedData[key];
+    }
+  });
+
+  return prunedData;
+}
